6954662818fde0afabcffc1cec2b29b992ad4fa8
asamigentoku <gentoku.asami.9v@stu.hosei.ac.jp>
2026-05-10 00:06:36 +0900

    crudeを修正

--- backend/src/main/java/com/example/backend/config/H2ServerConfig.java
+++ backend/src/main/java/com/example/backend/config/H2ServerConfig.java
@@ -2,6 +2,7 @@ package com.example.backend.config;
 
 
 import org.h2.tools.Server;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +13,7 @@ public class H2ServerConfig {
 
     // 開発環境(dev)やローカル環境でのみ起動するようにしておくと安全です
     //本番環境では削除
+    @ConditionalOnProperty(name = "h2.tcp.server.enabled", havingValue = "true")
     @Bean(initMethod = "start", destroyMethod = "stop")
     public Server h2TcpServer() throws SQLException {
         // ポート9092でTCPサーバーを起動し、外部ツールからの接続を許可する
--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -2,9 +2,14 @@ package com.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootTest
 class BackendApplicationTests {
+
+	@MockBean
+	StringRedisTemplate stringRedisTemplate;
 
 	@Test
 	void contextLoads() {
